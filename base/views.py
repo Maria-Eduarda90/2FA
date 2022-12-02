fd19b7605f395dff11b2c0abd3e0069252c35ea6
Mary Skywalker <snowdennaruto90@gmail.com>
2022-12-02 19:35:41 -0400

    login function structure

--- base/views.py
+++ base/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def register(request):
-    return HttpResponse("Hello word!")
+    return HttpResponse("register")
+
+
+def login(request):
+    return HttpResponse("login")
