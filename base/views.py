3676d44088232b3896ba06a59f288e286e799405
Mary Skywalker <snowdennaruto90@gmail.com>
2022-12-03 19:52:19 -0400

    getting input values

--- base/views.py
+++ base/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .utils import password_is_valid
+from django.shortcuts import redirect
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == "GET":
+        return render(request, 'register.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        if not password_is_valid(request, password, confirm_password):
+            return redirect('/auth/register')
+
+        return HttpResponse('Testando')
 
 
 def login(request):
