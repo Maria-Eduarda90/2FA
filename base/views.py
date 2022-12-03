aecae0b6fe07b069020dc4e41a58f56c0b498298
Mary Skywalker <snowdennaruto90@gmail.com>
2022-12-02 22:59:05 -0400

    rendering the register page

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def register(request):
-    return HttpResponse("register")
+    return render(request, 'register.html')
 
 
 def login(request):
